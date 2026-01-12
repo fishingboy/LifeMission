3dfa495feb519ebd1c7e246eb12d6efb09da56c7
Leo Kuo <et282523@hotmail.com>
2026-01-12 13:15:14 +0800

    加上 controller

---
+++ app/controller/ping.go
@@ -0,0 +1,13 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Ping(c *gin.Context) {
+	// Return JSON response
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
+}
--- app/router/ping.go
+++ app/router/ping.go
@@ -1,16 +1,13 @@
 package router
 
 import (
+	"github.com/fishingboy/LifeMission/app/controller"
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
 func PingBooking(r *gin.Engine) {
 	// Define a simple GET endpoint
 	r.GET("/ping", func(c *gin.Context) {
-		// Return JSON response
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
+		controller.Ping(c)
 	})
 }
